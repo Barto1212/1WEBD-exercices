7be979b7d28e958c94c34b37b6227527093267a3
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-11 16:50:06 +0100

    fin exo 6

--- j3/src/ex05.js
+++ j3/src/ex05.js
@@ -3,3 +3,15 @@
  * elle devra ajouter un event listener sur le click du bouton ayant comme id "ex_05_btn"
  * au click du bouton, le div ayant comme id "ex_05_div" doit obtenir une classe "ex_05_btn_clicked"
  */
+
+function setupEventListeners() {
+  const button = document.getElementById("ex_05_btn");
+  if (button) {
+    button.addEventListener("click", () => {
+      const div = document.getElementById("ex_05_div");
+      if (div) {
+        div.classList.add("ex_05_btn_clicked");
+      }
+    });
+  }
+}
--- j3/src/ex06.js
+++ j3/src/ex06.js
@@ -1,5 +1,13 @@
 /**
  * créer une fonction replaceText avec un argument newText
  * à l'execution, elle devra trouver et remplacer le text de tous les elements ayant une classe "ex_06_replace_me" par celui reçu en parametre
- * 
+ *
  */
+
+
+function replaceText(newText) {
+  const elements = document.querySelectorAll(".ex_06_replace_me");
+  elements.forEach(function(element) {
+    element.textContent = newText;
+  });
+}
