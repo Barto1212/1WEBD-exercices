e5953ed5e98318521ca0a81a3aa07b8d369e4957
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-11 14:09:04 +0100

    toDolist ok avec bugs

--- exos-supplementaires/to-do-list/script.js
+++ exos-supplementaires/to-do-list/script.js
@@ -1,9 +1,5 @@
-const input = document.querySelector("#input");
-input.addEventListener("keyup", handleClick);
-
-function handleClick(event) {
-  console.log(event.target.value);
-}
+const form = document.querySelector("form");
+form.addEventListener("submit", handleClick);
 
 const toDoListArray = [
   "Faire la vaisselle",
@@ -12,18 +8,26 @@ const toDoListArray = [
   "Faire une sieste",
 ];
 
+function handleClick(event) {
+  event.preventDefault();
+  const value = document.querySelector("#input").value;
+  console.log(value);
+  toDoListArray.push(value);
+  injectTodoList(toDoListArray);
+}
+
 //
 // prends un tableau en paramètre et l'affiche sous forme de liste
 injectTodoList(toDoListArray);
 //
-//
-//
-//
-//
-//
 // injecte la liste
 function injectTodoList(listTextArray) {
   const list = document.querySelector("#list");
+  const elementsToRemove = list.querySelectorAll("li");
+  console.log(elementsToRemove);
+  elementsToRemove.forEach((element) => {
+    list.removeChild(element);
+  });
   listTextArray.forEach((itemText) => {
     const item = document.createElement("li");
     item.textContent = itemText;
