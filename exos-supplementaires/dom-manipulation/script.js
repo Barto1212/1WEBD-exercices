3531bf8d6268c5a6af3eccf10181bdcd22e5796b
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-11 12:03:08 +0100

    todolist

--- exos-supplementaires/dom-manipulation/script.js
+++ exos-supplementaires/dom-manipulation/script.js
@@ -1,22 +1,54 @@
-// https://github.com/Barto1212/1WEBD-exercices
-
-const toDoListArray = ["Faire la vaisselle", "Nettoyer", "Regarder une série"];
-
+// Ajout du titre :
 const titreDeMaPage = document.createElement("h1");
 titreDeMaPage.textContent = "To do list";
 titreDeMaPage.classList.add("title");
 injectInRoot(titreDeMaPage);
+//
+//
+//
+const toDoListArray = [
+  "Faire la vaisselle",
+  "Nettoyer",
+  "Regarder une série",
+  "Faire une sieste",
+];
+generateList(toDoListArray);
 
-const listContainer = document.createElement("ul");
+//
+// prends un tableau en paramètre et l'affiche sous forme de liste
+function generateList(listTextArray) {
+  // création de ul :
+  const listContainer = document.createElement("ul");
 
-const listItem = document.createElement("li");
-listContainer.appendChild(listItem);
+  // ajout des li dans mes ul :
+  listTextArray.forEach((itemText) => {
+    const item = document.createElement("li");
+    item.textContent = itemText;
+    listContainer.appendChild(item);
+  });
 
-injectInRoot(listContainer);
+  // injection du ul contenant les li :
+  injectInRoot(listContainer);
+}
+//
+//
+//
+//
+//
+//
+//
 
+//
+// prends en argument un élément DOM et l'injecte dans root
 function injectInRoot(element) {
   // On séléctionne root :
   const root = document.querySelector("#root");
-  // On injecte :
-  root.appendChild(element);
+  // test si root existe
+  if (root) {
+    // On injecte :
+    root.appendChild(element);
+  } else {
+    // Sinon erreur
+    throw new Error("Root n'est pas définit");
+  }
 }
