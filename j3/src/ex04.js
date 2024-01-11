5368c183c7798c7d8421bc2a5f5771e977c23671
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-11 09:10:19 +0100

    new exo

--- j3/src/ex04.js
+++ j3/src/ex04.js
@@ -1,3 +1,10 @@
+function createElement() {
+  const newParagraph = document.createElement("p");
+  newParagraph.setAttribute("id", "ex_04_p");
+  newParagraph.innerText = "Hello World!";
+  document.body.appendChild(newParagraph);
+}
+
 /**
  * écrire une fonction createElement
  * cette fonction doit créer un nouveau paragraphe ayant comme id "ex_04_p" et l'ajouter au body
