6421559c32692c3fa8f32465c20ca5efb397f70d
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-11 10:57:03 +0100

    add css

--- exos-supplementaires/dom-manipulation/script.js
+++ exos-supplementaires/dom-manipulation/script.js
@@ -1,3 +1,22 @@
 // https://github.com/Barto1212/1WEBD-exercices
 
-const toDoListArray = ["Faire la vaisselle", "Nettoyer", "Regarder une série"]
+const toDoListArray = ["Faire la vaisselle", "Nettoyer", "Regarder une série"];
+
+const titreDeMaPage = document.createElement("h1");
+titreDeMaPage.textContent = "To do list";
+titreDeMaPage.classList.add("title");
+injectInRoot(titreDeMaPage);
+
+const listContainer = document.createElement("ul");
+
+const listItem = document.createElement("li");
+listContainer.appendChild(listItem);
+
+injectInRoot(listContainer);
+
+function injectInRoot(element) {
+  // On séléctionne root :
+  const root = document.querySelector("#root");
+  // On injecte :
+  root.appendChild(element);
+}
