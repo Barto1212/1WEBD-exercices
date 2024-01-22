41e689169014bea6e8dad61b866e5c8a0d467865
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-22 10:15:26 +0100

    refactor

--- cookie-clicker/script.js
+++ cookie-clicker/script.js
@@ -1,26 +1,23 @@
-// html et
-// style CSS : hover property
-// un beau titre centré
-
-// un fichier JS principal avec une fonction handleClick
-
 // un fichier avec une fonction qui enregistre dans le local storage les datas users: {
 // cookies: Number,
 //   grandMa: Number
 // }
 // une seconde fonction dans ce fichier qui accède au localStorage
 
-let CookieNum = 0
-document.getElementById('cookie-count').innerHTML = `cookie : ${CookieNum}`
+let CookieNum = 0;
+document.getElementById("cookie-count").innerHTML = `cookie : ${CookieNum}`;
 
+const clickButton = document.querySelector("#cookie-button");
 
-const FG = document.getElementsByClassName('cookie-button')
+function addCookie() {
+  CookieNum++;
+}
 
-FG[0].addEventListener('click',
-    (e) => {
-        CookieNum ++
-        document.getElementById('cookie-count').innerHTML = `cookie : ${CookieNum}`
-    }
-    )
+function refresh() {
+  document.getElementById("cookie-count").innerHTML = `cookie : ${CookieNum}`;
+}
 
-
+clickButton.addEventListener("click", (e) => {
+  addCookie();
+  refresh();
+});
