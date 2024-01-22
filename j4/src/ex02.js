520243b233ec075231d3d3dd9c758042b7ada48f
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-22 15:48:10 +0100

    consignes

--- j4/src/ex02.js
+++ j4/src/ex02.js
@@ -7,7 +7,24 @@
  * - ajouter un eventListener sur le bouton#ex_02_btn pour que, a chaque click, il change l'image #ex_02_img avec les données de l'api mentionné plus haut
  *  */
 
-// ajouter l'eventListener :
-
+function setupDogPic() {
+  const button = document.querySelector("#ex_02_btn");
+  if (button) {
+    button.addEventListener("click", handleClick);
+  }
+  handleClick();
+}
 
 // Fonction appelée par l'event listener
+function handleClick() {
+  getImg().then((dogUrl) => {
+    const image = document.querySelector("#ex_02_img");
+    image.setAttribute("src", dogUrl);
+  });
+}
+
+async function getImg() {
+  const data = await fetch("https://dog.ceo/api/breeds/image/random");
+  const response = await data.json();
+  return response.message;
+}
