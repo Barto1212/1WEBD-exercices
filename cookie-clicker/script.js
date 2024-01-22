f518b95ae5241b5897af6c7543031fe6f75407bb
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-22 09:18:49 +0100

    script

---
+++ cookie-clicker/script.js
@@ -0,0 +1,11 @@
+// html et
+// style CSS : hover property
+// un beau titre centré
+
+// un fichier JS principal avec une fonction handleClick
+
+// un fichier avec une fonction qui enregistre dans le local storage les datas users: {
+// cookies: Number,
+//   grandMa: Number
+// }
+// une seconde fonction dans ce fichier qui accède au localStorage
