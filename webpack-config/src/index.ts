1a5860ad0f1b3400f8ef41f647a6690fe9b36073
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-01-25 12:30:36 +0100

    fetch movies

--- webpack-config/src/index.ts
+++ webpack-config/src/index.ts
@@ -1,3 +1,21 @@
 import "./index.css";
+import { getTrendMovies } from "./getTrendMovies";
 
-console.log("js is running");
+getTrendMovies().then((movies) => {
+  render(movies);
+});
+
+function render(movieList: string[]) {
+  const list = document.querySelector("#movie-list");
+
+  const elementsToRemove = list?.querySelectorAll("li");
+  elementsToRemove?.forEach((element) => {
+    list?.removeChild(element);
+  });
+
+  movieList.forEach((itemText) => {
+    const item = document.createElement("li");
+    item.textContent = itemText;
+    list?.appendChild(item);
+  });
+}
